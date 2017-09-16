9f4c00000872ee37541f07731c73995f363ffc1e
Armando <salixonline@gmail.com>
2017-09-16 02:34:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,6 +23,6 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
-  // return array;
+  array.shift();
+  return array;
 }
