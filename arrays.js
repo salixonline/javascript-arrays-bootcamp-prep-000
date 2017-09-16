9d453d578192284129f18061e0d0f6856b2bb77c
Armando <salixonline@gmail.com>
2017-09-16 02:27:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,6 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 */
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
