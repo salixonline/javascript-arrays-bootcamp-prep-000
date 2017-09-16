3de4fbf9acbb98d0e438d4e3695445b6051829b0
Armando <salixonline@gmail.com>
2017-09-16 02:50:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array;
 }
 
-// function removeElementFromEndOfArray(array) {
-//   return array.slice(0, array.length - 1);
-// }
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
